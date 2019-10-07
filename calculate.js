7d3f1de5db343e28a7d0d0a1d00d8f028f52a7d2
Sung Paik <sung.paik@codeimmersives.com>
2019-10-07 14:08:59 -0400

    finished calculate operation

--- calculate.js
+++ calculate.js
@@ -1,6 +1,38 @@
-function calculate() {
-  
+function calculate(num1, num2, operator) {
+    const convertNum1 = Number(num1);
+    const convertNum2 = Number(num2);
+    switch (operator) {
+        case '+':
+        case 'plus':
+        return result = convertNum1 + convertNum2;
+        break;
+    
+        case '-':
+        case 'minus':
+        return result = convertNum1 - convertNum2;
+        break;
+    
+        case 'x':
+        case 'X':
+        case 'times':
+        return result = convertNum1 * convertNum2;
+        break;
+    
+        case '/':
+        return result = convertNum1 / convertNum2;
+        break;
+
+        case '%':
+        case 'mod':
+        case 'modulus':
+        return result = convertNum1 % convertNum2;
+
+        default: return 'Sorry, that\'s not a mathematical operation!'
+    }
 }
+
+calculate(3, 5, '+')
+
 
 
 module.exports = calculate;
